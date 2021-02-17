88c840d03ddfc75d9ae6d58d2a41964f9590e300
Ganesh C K M <ckmganesh2000@gmail.com>
2021-02-17 18:31:04 +0530

    Added Procfile and requirements.txt

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 
 import numpy as np
-import pandas as pd
-from io import BytesIO
 import tensorflow.keras
-import numpy as np
 from PIL import Image, ImageOps
 from flask import Flask, render_template, request
-import numpy as np
 from flask_cors import CORS, cross_origin
 
 
--- test.py
+++ test.py
@@ -1,13 +1,9 @@
 import requests
-import time
 
 '''
 your localhost url. If running on port 5000
 '''
 url = "http://localhost:5000/process"
-# Path to image file
 filess = {"img": open("D:/DATASET/TEST/R/R_10008.jpg", "rb")}
-starttime = time.time()
 results = requests.post(url, files=filess)
-print("time taken:", time.time() - starttime)
 print(results.text)
